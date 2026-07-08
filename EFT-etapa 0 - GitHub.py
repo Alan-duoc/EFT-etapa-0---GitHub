0006c874f5d4a600783030d9e460747d5eaec9a4
Alan-duoc <ala.diazd@duocuc.cl>
2026-07-07 20:25:49 -0400

    cascaron del codigo

---
+++ EFT-etapa 0 - GitHub.py
@@ -0,0 +1,50 @@
+import os
+productos = {
+    "P101": ["Cuaderno", "Papelería", 2490, True],
+    "P102": ["Lápiz", "Papelería", 590, True],
+    "P103": ["Botella", "Accesorios", 6990, False],
+    "P104": ["Mochila", "Accesorios", 24990, True]
+}
+
+inventario = {
+    "P101": [30, 15],
+    "P102": [120, 50],
+    "P103": [0, 10],
+    "P104": [8, 25]
+}
+
+lista_def = []
+#_________________________________________________________________
+#validaciones
+def validar_nombre(nombre):
+    pass
+def validar_categoria(categoria):
+    pass
+def validar_precio(precio):
+    pass
+def validar_disponible():
+    pass
+#_________________________________________________________________
+#code 
+def menu_prin():
+    print('''========== MENÚ PRINCIPAL ==========
+1. Stock por categoría
+2. Buscar productos por rango de precio
+3. Actualizar precio 4. Agregar producto
+5. Eliminar producto
+6. Mostrar productos
+7. Salir
+===================================
+''')
+def leer_opc():
+    pass
+def buscar_producto():
+    pass
+def actualizar_precio():
+    pass
+def agregar_producto():
+    pass
+def eliminar_producto():
+    pass
+def mostrar_producto():
+    pass
